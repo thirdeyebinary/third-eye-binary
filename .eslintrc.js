c12ebf69dd1c78489feb572569723c05f25769f7
thirdeyebinary <geraldbellico@gmail.com>
2024-12-11 15:44:06 -0500

    replaced eslint config with nuclear option 3.0

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    root: true,
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off'
+    },
+    ignorePatterns: ['**/*']
+  }
