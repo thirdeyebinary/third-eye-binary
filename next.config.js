db9d8d5ffc98eae1a2834d120b84e9779d5a2bf1
thirdeyebinary <geraldbellico@gmail.com>
2024-12-11 15:35:53 -0500

    updated next config to ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 }
 
 module.exports = nextConfig
